73b8adae60140af2a6fda5a8ad8214c8c6201c7e
Minyus <yusuke.minami86@gmail.com>
2019-10-13 16:09:27 +0800

    Support Kaggle Kernel

--- kaggle/src/kaggle_nfl/kaggle_nfl.py
+++ kaggle/src/kaggle_nfl/kaggle_nfl.py
@@ -3,39 +3,38 @@ import numpy as np
 import sys
 import os
 from pathlib import Path
-from skew_scaler import SkewScaler
-import logging
 
-log = logging.getLogger(__name__)
+if sys.version_info >= (3, 6, 8):
+    from skew_scaler import SkewScaler
 
 
 def fit_base_model(df, parameters):
     play_df = df.drop_duplicates(subset="PlayId")
+
     model = SkewScaler()
+
     model.fit(play_df["Yards"])
+
     return model
 
 
 def infer(model, parameters):
     from kaggle.competitions import nflrush
 
-    log.info("Started inference.")
-
     env = nflrush.make_env()
     for (test_df, sample_prediction_df) in env.iter_test():
         sample_prediction_df.iloc[0, :] = model.cdf(np.arange(-99, 100, 1))
         env.predict(sample_prediction_df)
         if sys.version_info >= (3, 6, 8):
-            log.info("Completed 1st inference iteration. Skip the rest.")
             return sample_prediction_df
 
     env.write_submission_file()
-    log.info("Completed inference.")
-    return sample_prediction_df
+
+    return None
 
 
 if __name__ == "__main__":
-    if sys.version_info >= (3, 6, 8):
+    if sys.version_info == (3, 6, 8):
         log.info("Completed 1st inference iteration. Skip the rest.")
         project_path = Path(__file__).resolve().parent.parent.parent
 
