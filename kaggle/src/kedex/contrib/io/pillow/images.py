7b22dc9c1c1a543db18080a85e349abf26d32bdc
Minyus <yusuke.minami86@gmail.com>
2019-10-16 09:26:00 +0800

    Add scaling feature with lower and upper to ImagesLocalDataSet

--- kaggle/src/kedex/contrib/io/pillow/images.py
+++ kaggle/src/kedex/contrib/io/pillow/images.py
@@ -36,6 +36,9 @@ class ImagesLocalDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
         load_args = load_args or dict()
 
         as_numpy = load_args.pop("as_numpy", None)
+        upper = load_args.pop("upper", None)
+        lower = load_args.pop("lower", None)
+        to_scale = upper or lower
 
         if load_path.is_dir():
             images_dict = {}
@@ -43,21 +46,35 @@ class ImagesLocalDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
                 with p.open("r") as local_file:
                     img = Image.open(local_file, **load_args)
                     images_dict[p.stem] = img
-            if as_numpy:
+            if as_numpy or to_scale:
                 images = list(images_dict.values())
                 names = list(images_dict.keys())
 
                 images = [np.asarray(img) for img in images]
                 images = np.stack(images, axis=0)
-                images_dict = dict(images=images, names=names)
+
+                images = scale(images, lower=lower, upper=upper)
+
+                if as_numpy:
+                    images_dict = dict(images=images, names=names)
+
+                if not as_numpy:
+                    for i in range(images.shape[0]):
+                        img = Image.fromarray(images[i, :, :, :])
+                        images_dict[names[i]] = img
 
             return images_dict
 
         else:
             with load_path.open("r") as local_file:
                 img = Image.open(local_file, **load_args)
-                if as_numpy:
+                if as_numpy or to_scale:
                     img = np.asarray(img)
+                    img = scale(img, lower=lower, upper=upper)
+
+                    if not as_numpy:
+                        img = Image.fromarray(img)
+
                 return img
 
     def _save(self, data: Union[np.ndarray, dict, type(Image)]) -> None:
@@ -67,20 +84,31 @@ class ImagesLocalDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
         save_args = copy.deepcopy(self._save_args)
         save_args = save_args or dict()
         mode = save_args.pop("mode", None)
+        upper = save_args.pop("upper", None)
+        lower = save_args.pop("lower", None)
+        to_scale = upper or lower
 
         save_path.parent.mkdir(parents=True, exist_ok=True)
 
         if isinstance(data, type(Image)):
-            data.save(p, **save_args)
-            return None
+            if to_scale:
+                images = np.asarray(data)
+            else:
+                data.save(p, **save_args)
+                return None
 
         elif isinstance(data, list):
-            p.mkdir(parents=True, exist_ok=True)
-            for i, img in enumerate(data):
-                assert isinstance(img, type(Image))
-                s = p / "{}_{:05d}{}".format(p.stem, i, p.suffix)
-                img.save(s, **save_args)
-                return None
+            if to_scale:
+                images = [np.asarray(img) for img in data]
+                images = np.stack(images, axis=0)
+
+            else:
+                p.mkdir(parents=True, exist_ok=True)
+                for i, img in enumerate(data):
+                    assert isinstance(img, type(Image))
+                    s = p / "{}_{:05d}{}".format(p.stem, i, p.suffix)
+                    img.save(s, **save_args)
+                    return None
 
         elif isinstance(data, np.ndarray):
             images = data
@@ -93,6 +121,8 @@ class ImagesLocalDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
         else:
             raise ValueError("Unsupported data type: {}".format(type(data)))
 
+        images = scale(images, lower=lower, upper=upper)
+
         if images.ndim in {2, 3}:
             img = Image.fromarray(images, mode=mode)
             img.save(p, **save_args)
@@ -100,12 +130,11 @@ class ImagesLocalDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
 
         elif images.ndim == 4:
             p.mkdir(parents=True, exist_ok=True)
-            total = images.shape[0]
+            for i in range(images.shape[0]):
+                img = Image.fromarray(images[i, :, :, :], mode=mode)
 
-            for i in range(total):
                 name = names[i] if names else "{:05d}".format(i)
                 s = p / "{}_{}{}".format(p.stem, name, p.suffix)
-                img = Image.fromarray(images[i, :, :, :], mode=mode)
                 img.save(s, **save_args)
             return None
 
@@ -126,3 +155,17 @@ class ImagesLocalDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
         except DataSetError:
             return False
         return Path(path).exists()
+
+
+def scale(a, lower=None, upper=None):
+    if lower or upper:
+        max_val = a.max()
+        min_val = a.min()
+        stat_dict = dict(max_val=max_val, min_val=min_val)
+        log.info(stat_dict)
+        upper = upper or max_val
+        lower = lower or min_val
+        a = (((a - min_val) / (max_val - min_val)) * (upper - lower) + lower).astype(
+            np.uint8
+        )
+    return a
