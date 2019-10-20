faf52bc051f924a9b162b634d5a256665f4653d4
Minyus <yusuke.minami86@gmail.com>
2019-10-21 07:18:35 +0800

    Make it stand-alone

--- kaggle/src/kaggle_nfl/kaggle_nfl.py
+++ kaggle/src/kaggle_nfl/kaggle_nfl.py
@@ -494,6 +494,190 @@ def compose(transforms):
     return _compose
 
 
+""" pytorch_ops.py """
+import torch
+
+from torch.utils.data import DataLoader
+from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
+from ignite.metrics import RunningAverage
+from ignite.contrib.handlers.tqdm_logger import ProgressBar
+import numpy as np
+from pkg_resources import parse_version
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def pytorch_train(
+    train_params,  # type: dict
+    mlflow_logging=True,  # type: bool
+):
+    if mlflow_logging:
+        try:
+            import mlflow
+        except ImportError:
+            log.warning("Failed to import mlflow. MLflow logging is disabled.")
+            mlflow_logging = False
+
+    if mlflow_logging:
+        import ignite
+
+        if parse_version(ignite.__version__) >= parse_version("0.2.1"):
+            from ignite.contrib.handlers.mlflow_logger import (
+                MLflowLogger,
+                OutputHandler,
+                global_step_from_engine,
+            )
+        else:
+            from .ignite.contrib.handlers.mlflow_logger import (
+                MLflowLogger,
+                OutputHandler,
+                global_step_from_engine,
+            )
+
+    def _pytorch_train(model, train_dataset, val_dataset=None, parameters=None):
+
+        train_data_loader_params = train_params.get("train_data_loader_params", dict())
+        val_data_loader_params = train_params.get("val_data_loader_params", dict())
+        epochs = train_params.get("epochs")
+        progress_update = train_params.get("progress_update", dict())
+
+        optim = train_params.get("optim")
+        optim_params = train_params.get("optim_params", dict())
+        loss_fn = train_params.get("loss_fn")
+        metrics = train_params.get("metrics")
+
+        evaluate_train_data = train_params.get("evaluate_train_data")
+        evaluate_val_data = train_params.get("evaluate_val_data")
+
+        seed = train_params.get("seed")
+        cudnn_deterministic = train_params.get("cudnn_deterministic")
+        cudnn_benchmark = train_params.get("cudnn_benchmark")
+
+        if seed:
+            torch.manual_seed(seed)
+            np.random.seed(seed)
+        if cudnn_deterministic:
+            torch.backends.cudnn.deterministic = cudnn_deterministic
+        if cudnn_benchmark:
+            torch.backends.cudnn.benchmark = cudnn_benchmark
+
+        optimizer = optim(model.parameters(), **optim_params)
+
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        trainer = create_supervised_trainer(
+            model, optimizer, loss_fn=loss_fn, device=device
+        )
+
+        train_data_loader_params.setdefault("shuffle", True)
+        train_data_loader_params.setdefault("drop_last", True)
+        train_loader = DataLoader(train_dataset, **train_data_loader_params)
+
+        if evaluate_train_data:
+            evaluator_train = create_supervised_evaluator(
+                model, metrics=metrics, device=device
+            )
+
+        if evaluate_val_data:
+            val_loader = DataLoader(val_dataset, **val_data_loader_params)
+            evaluator_val = create_supervised_evaluator(
+                model, metrics=metrics, device=device
+            )
+
+        pbar = None
+        if isinstance(progress_update, dict):
+            RunningAverage(output_transform=lambda x: x).attach(trainer, "loss")
+
+            progress_update.setdefault("persist", True)
+            progress_update.setdefault("desc", "")
+            pbar = ProgressBar(**progress_update)
+            pbar.attach(trainer, ["loss"])
+
+        @trainer.on(Events.EPOCH_COMPLETED)
+        def log_evaluation_results(engine):
+            if evaluate_train_data:
+                evaluator_train.run(train_loader)
+                if pbar:
+                    pbar.log_message(
+                        _get_report_str(engine, evaluator_train, "Train Data")
+                    )
+            if evaluate_val_data:
+                evaluator_val.run(val_loader)
+                if pbar:
+                    pbar.log_message(_get_report_str(engine, evaluator_val, "Val Data"))
+
+        if mlflow_logging:
+            mlflow_logger = MLflowLogger()
+
+            logging_params = {
+                "train_n_samples": len(train_dataset),
+                "val_n_samples": len(val_dataset),
+                "optim": optim.__name__,
+                "loss_fn": loss_fn.__name__,
+                "pytorch_version": torch.__version__,
+                "ignite_version": ignite.__version__,
+            }
+            logging_params.update(_loggable_dict(train_data_loader_params, "train"))
+            logging_params.update(_loggable_dict(val_data_loader_params, "val"))
+            logging_params.update(_loggable_dict(optim_params))
+            mlflow_logger.log_params(logging_params)
+
+            if evaluate_train_data:
+                mlflow_logger.attach(
+                    evaluator_train,
+                    log_handler=OutputHandler(
+                        tag="train",
+                        metric_names=list(metrics.keys()),
+                        global_step_transform=global_step_from_engine(trainer),
+                    ),
+                    event_name=Events.EPOCH_COMPLETED,
+                )
+            if evaluate_val_data:
+                mlflow_logger.attach(
+                    evaluator_val,
+                    log_handler=OutputHandler(
+                        tag="val",
+                        metric_names=list(metrics.keys()),
+                        global_step_transform=global_step_from_engine(trainer),
+                    ),
+                    event_name=Events.EPOCH_COMPLETED,
+                )
+
+        trainer.run(train_loader, max_epochs=epochs)
+
+        return model
+
+    return _pytorch_train
+
+
+def _get_report_str(engine, evaluator, tag=""):
+    report_str = "[Epoch: {} | {} | Metrics: {}]".format(
+        engine.state.epoch, tag, evaluator.state.metrics
+    )
+    return report_str
+
+
+def _loggable_dict(d, prefix=None):
+    return {
+        ("{}_{}".format(prefix, k) if prefix else k): (
+            "{}".format(v) if isinstance(v, (tuple, list, dict, set)) else v
+        )
+        for k, v in d.items()
+    }
+
+
+class PytorchSequential(torch.nn.Sequential):
+    def __init__(self, modules):
+        super().__init__(*modules)
+
+
+class PytorchFlatten(torch.nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+
+""" """
+
 if __name__ == "__main__":
     if sys.version_info == (3, 6, 8):
         log.info("Completed 1st inference iteration. Skip the rest.")
@@ -512,7 +696,50 @@ if __name__ == "__main__":
     print("Preprocess.")
     df = preprocess(df)
     print("Fit model.")
-    model = fit_base_model(df, parameters)
+
+    import ignite
+
+    train_batch_size = 128
+    loss_fn = nfl_crps_loss
+    train_params = dict(
+        optim=torch.optim.SGD,
+        optim_params=dict(
+            lr=train_batch_size / 1000,
+            momentum=1 - train_batch_size / 2000,
+            weight_decay=0.1 / train_batch_size,
+        ),
+        loss_fn=loss_fn,
+        metrics=dict(loss=ignite.metrics.Loss(loss_fn=loss_fn)),
+        train_data_loader_params=dict(batch_size=train_batch_size, num_workers=4),
+        evaluate_train_data=False,
+        evaluate_val_data=False,
+        epochs=1,  # number of epochs to train
+        seed=0,  #
+    )
+
+    pytorch_model = torch.nn.Sequential(
+        torch.nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3),
+        torch.nn.ReLU(),
+        torch.nn.AdaptiveMaxPool2d(output_size=(14, 29)),
+        torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
+        torch.nn.ReLU(),
+        torch.nn.AdaptiveMaxPool2d(output_size=(6, 14)),
+        torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+        torch.nn.ReLU(),
+        torch.nn.AdaptiveMaxPool2d(output_size=1),
+        PytorchFlatten(),
+        torch.nn.Dropout(0.2),
+        torch.nn.Linear(in_features=64, out_features=2),
+        PytorchLogNormalCDF(x_start=1, x_end=200, x_scale=0.01),
+    )
+
+    train_dataset = FieldImagesDataset(df, transform=ToTensor())
+
+    model = pytorch_train(train_params=train_params, mlflow_logging=False)(
+        pytorch_model, train_dataset
+    )
+
+    # model = fit_base_model(df, parameters)
     print("Infer.")
     infer(model, parameters)
     print("Completed.")
