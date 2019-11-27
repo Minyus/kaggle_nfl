a5ae9cf42c7a4339cc3ce8f841caab70fba96ee7
Minyus <yusuke.minami86@gmail.com>
2019-11-28 04:12:14 +0800

    Use base_probas for each PossessionTeam

--- kaggle/src/kaggle_nfl/kaggle_nfl.py
+++ kaggle/src/kaggle_nfl/kaggle_nfl.py
@@ -436,7 +436,7 @@ def _relative_values(abs_sr, comp_sr, offset=101, transform_func=None):
 
 
 class BaseProbas:
-    def __init__(self, groupby=None, yards_query="-10 <= Yards < 40"):
+    def __init__(self, groupby=["PossessionTeam"], yards_query="-10 <= Yards < 40"):
         self.groupby = groupby
         self.yards_query = yards_query
         self.agg_df = None
@@ -707,7 +707,7 @@ class FieldImagesDataset:
 
                 """ Base probas """
                 if base_probas is not None:
-                    base_probas_df = base_probas.transform(rusher_df[["PlayIndex"]])
+                    base_probas_df = base_probas.transform(rusher_df[["PlayIndex", "PossessionTeam"]])
                     base_probas_df["Channel"] = dim_sizes_[0]
                     melted_si_df = df_concat()(melted_si_df, base_probas_df)
 
